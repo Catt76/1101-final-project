cc5fcc9be07f96d43d5caaf91e9bd8500ce1b4a6
Catherine <catherine.martinez04@utrgv.edu>
2023-11-29 16:31:42 -0600

    Added front-end code

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-# First Commit
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(
+    debug=True
+)
+
+
 
 
